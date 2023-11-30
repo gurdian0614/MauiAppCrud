63c104b285cb1f230f2703a5751c58371d8af6ff
gurdian0614 <ivan.gurdian@outlook.com>
2023-11-29 19:06:06 -0600

    Creando clase empleado mensaje

---
+++ MauiAppCrud/Utilities/EmpleadoMensaje.cs
@@ -0,0 +1,10 @@
+﻿using MauiAppCrud.DTO;
+
+namespace MauiAppCrud.Utilities
+{
+    public class EmpleadoMensaje
+    {
+        public bool crear { get; set; }
+        public EmpleadoDTO EmpleadoDTO { get; set; }
+    }
+}
