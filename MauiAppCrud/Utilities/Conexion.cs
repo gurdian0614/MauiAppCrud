264ddf6cb61d9d518b6a5d95515282650bed1110
gurdian0614 <ivan.gurdian@outlook.com>
2023-11-29 19:05:47 -0600

    Creando clase estatica conexion

---
+++ MauiAppCrud/Utilities/Conexion.cs
@@ -0,0 +1,24 @@
+﻿
+namespace MauiAppCrud.Utilties
+{
+    public static class Conexion
+    {
+        public static string ObtenerRuta(string nombreDB)
+        {
+            string ruta = string.Empty;
+            string env = string.Empty;
+
+            if (DeviceInfo.Platform == DevicePlatform.Android)
+            {
+                env = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                ruta = Path.Combine(env, nombreDB);
+            } else if (DeviceInfo.Platform == DevicePlatform.iOS)
+            {
+                env = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                ruta = Path.Combine (env, "..","Library", nombreDB);
+            }
+
+            return ruta;
+        }
+    }
+}
