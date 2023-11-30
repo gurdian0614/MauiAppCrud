7fc8dbfe0cd4bef87ab367a1d2ebfe0522538ec1
gurdian0614 <ivan.gurdian@outlook.com>
2023-11-29 19:03:28 -0600

    Creando contexto db

---
+++ MauiAppCrud/DataAccess/EmpleadoDbContext.cs
@@ -0,0 +1,26 @@
+﻿using MauiAppCrud.Models;
+using MauiAppCrud.Utilties;
+using Microsoft.EntityFrameworkCore;
+
+namespace MauiAppCrud.DataAccess
+{
+    public class EmpleadoDbContext : DbContext
+    {
+        public DbSet<Empleado> EmpleadoSet { get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string conexion = $"FileName={Conexion.ObtenerRuta("empleado.db")}";
+            optionsBuilder.UseSqlite(conexion);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Empleado>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Id).IsRequired().ValueGeneratedOnAdd();
+            });
+        }
+    }
+}
