159bc3a5c25f231a4757f93bb01b8369d5da52fe
gurdian0614 <ivan.gurdian@outlook.com>
2023-11-29 19:03:40 -0600

    Creando DTO empleado

---
+++ MauiAppCrud/DTO/EmpleadoDTO.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MauiAppCrud.DTO
+{
+    public partial class EmpleadoDTO : ObservableObject
+    {
+        [ObservableProperty]
+        public int id;
+        [ObservableProperty]
+        public string nombre; 
+        [ObservableProperty] 
+        public string email;
+        [ObservableProperty]
+        public decimal sueldo;
+        [ObservableProperty]
+        public DateTime fechaContrato;
+    }
+}
