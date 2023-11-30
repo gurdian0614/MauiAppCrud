8af62fb24e444ac8361792ede9a0a5d65f6bc9e5
gurdian0614 <ivan.gurdian@outlook.com>
2023-11-29 19:05:33 -0600

    Creando modelo empleado

---
+++ MauiAppCrud/Models/Empleado.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MauiAppCrud.Models
+{
+    public class Empleado
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Email { get; set; }
+
+        public decimal Sueldo { get; set; }
+
+        public DateTime FechaContrato { get; set; }
+    }
+}
